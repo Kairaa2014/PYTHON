28c08d888f276b7fa316ebd266b8a580a34c82f8
Kairaa2014 <kairaasharma22@gmail.com>
2024-08-18 13:00:00 +0800

    Add files via upload

---
+++ Python notes.py
@@ -0,0 +1,11 @@
+# Chap 1 - Variables & Data types
+# Chap 2 - Strings & conditional statements 
+# Chap 3 - List and tuples 
+# Chap 4 - Dictionary & set
+# Chap 5 - Loops
+# Chap 6 - Functions & Recursion
+# Chap 7 - File i / o
+# Chap 8 - OOPS
+
+# CHAP 1
+
